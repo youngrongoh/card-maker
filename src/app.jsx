421ef9436c4d8cf07faf45b84badb6ae4bec770e
YoungRong.Oh <youngrong.oh@gmail.com>
2021-02-09 16:28:54 +0900

    apply react-router

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,21 @@
 import styles from './app.module.css';
 import Login from './login/login';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Maker from './maker/maker';
 
 function App({ authService }) {
   return (
     <div className={styles.app}>
-      <Login authService={authService} />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Login authService={authService} />
+          </Route>
+          <Route path="/maker">
+            <Maker authService={authService} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
