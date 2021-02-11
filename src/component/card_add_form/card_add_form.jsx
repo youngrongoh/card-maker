25e17289e0048fb7c738e83e6bbc06f40d025c34
YoungRong.Oh <youngrong.oh@gmail.com>
2021-02-11 21:11:21 +0900

    add CardAddForm component

---
+++ src/component/card_add_form/card_add_form.jsx
@@ -0,0 +1,83 @@
+import React, { useRef } from 'react';
+import styles from './card_add_form.module.css';
+import Button from '../button/button';
+import ImageFileInput from '../image_file_input/image_file_input';
+
+const CardAddForm = ({ onAdd }) => {
+  const formRef = useRef();
+  const nameRef = useRef();
+  const companyRef = useRef();
+  const themeRef = useRef();
+  const titleRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const card = {
+      id: Date.now(),
+      name: nameRef.current.value || '',
+      company: companyRef.current.value || '',
+      theme: themeRef.current.value || '',
+      title: titleRef.current.value || '',
+      email: emailRef.current.value || '',
+      message: messageRef.current.value || '',
+      fileName: '',
+      fileURL: '',
+    };
+    formRef.current.reset();
+    onAdd(card);
+  };
+
+  return (
+    <form ref={formRef} className={styles.form}>
+      <input
+        ref={nameRef}
+        className={styles.input}
+        type="text"
+        name="name"
+        placeholder="Name"
+      />
+      <input
+        ref={companyRef}
+        className={styles.input}
+        type="text"
+        name="company"
+        placeholder="Company"
+      />
+      <select ref={themeRef} className={styles.select} name="theme">
+        <option value="dark">Dark</option>
+        <option value="light">Light</option>
+        <option value="colorful">Colorful</option>
+      </select>
+      <input
+        ref={titleRef}
+        className={styles.input}
+        type="text"
+        name="title"
+        placeholder="Title"
+      />
+      <input
+        ref={emailRef}
+        className={styles.input}
+        type="text"
+        name="email"
+        placeholder="Email"
+      />
+      <textarea
+        ref={messageRef}
+        className={styles.textarea}
+        name="message"
+        placeholder="Message"
+      ></textarea>
+      <div className={styles.fileInput}>
+        <ImageFileInput name="no file" />
+      </div>
+      <div className={styles.button}>
+        <Button name="Add" onClick={onSubmit} />
+      </div>
+    </form>
+  );
+};
+
+export default CardAddForm;
--- src/component/editor/editor.jsx
+++ src/component/editor/editor.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import CardAddForm from '../card_add_form/card_add_form';
 import CardEditForm from '../card_edit_form/card_edit_form';
 import styles from './editor.module.css';
 
-const Editor = ({ cards }) => {
+const Editor = ({ cards, addCard }) => {
   return (
     <section className={styles.editor}>
       <h1 className={styles.title}>Card Editor</h1>
       <ul className={styles.list}>
         {cards.map((card) => {
           return (
-            <li className={styles.item}>
+            <li key={card.id} className={styles.item}>
               <CardEditForm card={card} />
             </li>
           );
         })}
+        <CardAddForm onAdd={addCard} />
       </ul>
     </section>
   );
